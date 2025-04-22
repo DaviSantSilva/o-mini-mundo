678b1d1a513913e36dc2150d911aa2ad5931a37f
Davi Silva <davi200123@gmail.com>
2025-04-22 17:29:24 -0300

    creating controllers

---
+++ src/main/java/com/minimundo/controller/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.minimundo.controller;
+
+import com.minimundo.dto.AuthenticationRequest;
+import com.minimundo.dto.AuthenticationResponse;
+import com.minimundo.dto.RegisterRequest;
+import com.minimundo.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+} 
