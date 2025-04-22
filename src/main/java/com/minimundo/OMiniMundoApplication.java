cf09e458554e114a843920e9b5e8cc38040a4a09
Davi Silva <davi200123@gmail.com>
2025-04-22 16:19:59 -0300

    first commit

---
+++ src/main/java/com/minimundo/OMiniMundoApplication.java
@@ -0,0 +1,11 @@
+package com.minimundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OMiniMundoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OMiniMundoApplication.class, args);
+    }
+} 
